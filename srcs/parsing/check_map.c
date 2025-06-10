7fd71c5bfdce23c9e9a5e8c2e463dfeeecb3c29f
Brendan Victoire <bvictoir@f2r3s8.clusters.42paris.fr>
2025-06-10 10:35:08 +0200

    change strchr to ft_strchr

--- srcs/parsing/check_map.c
+++ srcs/parsing/check_map.c
@@ -6,7 +6,7 @@
 /*   By: bvictoir <bvictoir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/05 13:35:10 by bvictoir          #+#    #+#             */
-/*   Updated: 2025/06/10 10:20:18 by bvictoir         ###   ########.fr       */
+/*   Updated: 2025/06/10 10:34:22 by bvictoir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,13 +35,13 @@ static int	is_wall_floor(t_data *data, int i, int j)
 	if (!data->map[i - 1] || !data->map[i + 1] || !data->map[i][j - 1]
 		|| !data->map[i][j + 1])
 		return (2);
-	if (!strchr("NSWE10", data->map[i - 1][j]))
+	if (!ft_strchr("NSWE10", data->map[i - 1][j]))
 		return (2);
-	if (!strchr("NSWE10", data->map[i + 1][j]))
+	if (!ft_strchr("NSWE10", data->map[i + 1][j]))
 		return (2);
-	if (!strchr("NSWE10", data->map[i][j - 1]))
+	if (!ft_strchr("NSWE10", data->map[i][j - 1]))
 		return (2);
-	if (!strchr("NSWE10", data->map[i][j + 1]))
+	if (!ft_strchr("NSWE10", data->map[i][j + 1]))
 		return (2);
 	return (0);
 }
@@ -58,7 +58,7 @@ static int	check_interior(t_data *data, int end)
 		j = 1;
 		while (data->map[i][j])
 		{
-			if (strchr("NSWE", data->map[i][j]))
+			if (ft_strchr("NSWE", data->map[i][j]))
 				data->map[i][j] = set_player(data, i, j);
 			if (!(data->map[i][j] == '1' || data->map[i][j] == '0'
 					|| data->map[i][j] == ' '))
@@ -68,7 +68,7 @@ static int	check_interior(t_data *data, int end)
 					return (2);
 			j++;
 		}
-		if (!strchr("1 ", data->map[i][j - 1]))
+		if (!ft_strchr("1 ", data->map[i][j - 1]))
 			return (2);
 		i++;
 	}
@@ -82,14 +82,14 @@ static int	check_extreme(t_data *data, int nb)
 	i = 0;
 	while (data->map[nb][i])
 	{
-		if (!strchr("1 ", data->map[nb][i]))
+		if (!ft_strchr("1 ", data->map[nb][i]))
 			return (1);
 		i++;
 	}
 	i = 0;
 	while (data->map[i] && data->map[i][0])
 	{
-		if (!strchr("1 ", data->map[i][0]))
+		if (!ft_strchr("1 ", data->map[i][0]))
 			return (4);
 		i++;
 	}
