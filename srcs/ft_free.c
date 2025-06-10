6c2747915c56fce570e2314fd50cd5a96f8f7ea1
Brendan Victoire <bvictoir@f2r3s8.clusters.42paris.fr>
2025-06-10 11:50:51 +0200

    change ft_printf to write

--- srcs/ft_free.c
+++ srcs/ft_free.c
@@ -6,7 +6,7 @@
 /*   By: bvictoir <bvictoir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/27 10:57:19 by bvictoir          #+#    #+#             */
-/*   Updated: 2025/06/10 11:48:38 by bvictoir         ###   ########.fr       */
+/*   Updated: 2025/06/10 11:50:48 by bvictoir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,7 +71,7 @@ void	exit_error(t_data *data, char *mess)
 		mlx_destroy_display(data->mlx);
 		free(data->mlx);
 	}
-	write(2, msg, ft_strlen(msg)); 
+	write(2, mess, ft_strlen(mess)); 
 	write(2, "\n", 1);
 	exit(1);
 }
